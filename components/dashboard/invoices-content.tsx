2bafeb9aefc082e437040c82260c7e8f5d66a833
Vaibhav Kandharkar <vaibhavk0099#gmail.com>
2026-05-05 18:54:27 +0530

    Chnages

--- components/dashboard/invoices-content.tsx
+++ components/dashboard/invoices-content.tsx
@@ -423,7 +423,7 @@ body{
 <div class="institute">
 <h2>DNYANSAGAR CLASSES</h2>
 <p>201/A, New Excelsior Building Opp. Crown Hotel, KHADKI Pune - 411003 </p>
-<p>Phone no : 8180802049</p>
+<p>Phone no : 8862010906</p>
 
 <p>State: Maharashtra</p>
 </div>
